3f321e6899f029a7ad85604771aae507909dcce7
ravi-119 <ry277015@gmail.com>
2026-03-11 11:54:58 +0530

    requirements.txt is added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medibot',
+    version='0.1.0',    
+    author='Ravi Kumar',
+    description='A medical chatbot application using LangChain and Pinecone.',
+    author_email="ry277015@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
